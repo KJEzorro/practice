86d692417f1b1444db7a282bfb21b9c19df1d623
KJEzorro <gndlg91@gmail.com>
2022-05-10 19:09:01 +0900

    ,

--- app/src/main/java/practice01/Practice003.java
+++ app/src/main/java/practice01/Practice003.java
@@ -1,5 +1,7 @@
 package practice01;
 
+import java.util.Scanner;
+
 public class Practice003 {
 
   //  문제
@@ -12,6 +14,17 @@ public class Practice003 {
   //  첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+
+    System.out.println((a+b)%c);
+    System.out.println(((a%c) + (b%c)) % c);
+    System.out.println((a*b)%c);
+    System.out.println(((a%c) * (b%c)%c));
+
 
   }
 
