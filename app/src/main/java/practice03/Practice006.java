4fb88935a0288b2f7e7496c96d6d9b5a1bdc84ff
KJEzorro <gndlg91@gmail.com>
2022-05-31 19:22:39 +0900

    .

--- app/src/main/java/practice03/Practice006.java
+++ app/src/main/java/practice03/Practice006.java
@@ -1,5 +1,11 @@
 package practice03;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Practice006 {
   //  문제
   //  자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
@@ -9,6 +15,23 @@ public class Practice006 {
   //  첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
 
   public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    List<Integer> list = new ArrayList<>();
+
+    for (int i = 1; i <= n; i++) {
+      list.add(i);
+    }
+
+    Collections.reverse(list);
+
+    Iterator<Integer> it = list.iterator(); 
+    while (it.hasNext()) {
+      System.out.println(it.next());
+    }
+
 
   }
 
