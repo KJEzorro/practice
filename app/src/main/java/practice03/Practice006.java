ebf81b7832a11dc3feb097efe97e49316229d61f
KJEzorro <gndlg91@gmail.com>
2022-06-06 17:18:28 +0900

    .

--- app/src/main/java/practice03/Practice006.java
+++ app/src/main/java/practice03/Practice006.java
@@ -13,7 +13,6 @@ public class Practice006 {
   //  첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
   //  출력
   //  첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
-
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
