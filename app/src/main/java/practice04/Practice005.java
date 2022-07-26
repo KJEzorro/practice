a0fd6c80646d621c70b6f958f06618d39a131f34
KJEzorro <gndlg91@gmail.com>
2022-07-26 18:16:03 +0900

    >

--- app/src/main/java/practice04/Practice005.java
+++ app/src/main/java/practice04/Practice005.java
@@ -16,7 +16,6 @@ public class Practice005 {
   // 출력
   // 첫째 줄에 새로운 평균을 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-2 이하이면 정답이다.
 
-
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
